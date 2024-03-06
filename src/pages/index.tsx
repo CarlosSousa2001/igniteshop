f5491d23c986e6c45020bbbc02d966c1f2f98bc7
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-03-06 17:44:44 -0300

    erro ao usar ssg com paginas dinamicas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import 'keen-slider/keen-slider.css'
 import { stripe } from '../lib/stripe';
 import { GetServerSideProps, GetStaticProps } from 'next';
 import Stripe from 'stripe';
+import Link from 'next/link';
 
 const Button = styled('button', {
   backgroundColor: '$rocketseat',
@@ -37,14 +38,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products?.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
+          <Link href={`/product/${product.id}`} key={product.id} >
+          <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt="" />
 
             <footer>
               <strong>{product.name}</strong>
-              <span>{product.price}</span>
+              <span>{product.price.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</span>
             </footer>
           </Product>
+          </Link>
         )
       })}
     </HomeContainer>
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { ImageContainer, ProductContainer, ProductDetailsComponent } from '../../styles/pages/product'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+
+interface ProductsProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number,
+    description: string
+  }
+}
+
+export default function ProductDetails({ product }: ProductsProps) {
+
+  const { query } = useRouter()
+
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetailsComponent>
+        <h1>{product.name}</h1>
+        <span>{product.price.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</span>
+
+        <p>{product.description}</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetailsComponent>
+    </ProductContainer>
+  )
+}
+
+
+export const getStaticPaths = (async({params}) => {
+
+  const productId = params.id;
+
+  const item = await stripe.products.retrieve(productId, {
+    expand: ['data.default_price']
+  })
+
+  const price = item.default_price as Stripe.Price
+
+
+  return {
+    props:{
+      product:{
+        id: item.id,
+        name: item.name,
+        imageUrl: item.images[0],
+        price: price.unit_amount / 100,
+        description: item.description
+      }
+    },
+    revalidate: 3600
+  }
+
+}) 
