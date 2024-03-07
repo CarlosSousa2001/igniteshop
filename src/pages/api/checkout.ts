1505d0a6c31e2bac8c997dbd6736797006ba7b1e
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-03-07 11:07:19 -0300

    página de sucesso com striper

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import {NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({error: 'Price not found'})
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
@@ -31,3 +31,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     checkoutUrl: checkoutSession.url
   })
 }
+
+
+
